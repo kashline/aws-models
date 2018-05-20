84265fa8c2f816b51ee47be7b6741094ff572136
Kevin Ashline <kevin.ashline@sgreen-e7.corp.sd.local>
2018-05-20 13:31:12 -0400

    save

--- main.go
+++ main.go
@@ -1,19 +1,13 @@
 package main
 
-import (
-	"log"
-
-	"github.com/joho/godotenv"
-)
-
 func main() {
-	//NewInstance("ami-43a15f3e", "t2.micro", "Tester T. Testerson", 1, 1)
-	GetAmi()
+	NewInstance("ami-43a15f3e", "t2.micro", "Tester T. Testerson", 1, 1)
+
 }
 
-func init() {
-	err := godotenv.Load("local.env")
-	if err != nil {
-		log.Print("Failed to load local environment file")
-	}
-}
+// func init() {
+// 	err := godotenv.Load("local.env")
+// 	if err != nil {
+// 		log.Print("Failed to load local environment file")
+// 	}
+// }
